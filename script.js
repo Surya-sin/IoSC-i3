95c4b98af3a429d99c33cd77310d3da5011c9cf0
Surya <singhsurya1100@gmail.com>
2023-09-11 06:13:02 +0530

    website done

---
+++ script.js
@@ -0,0 +1,10 @@
+// Get the theme toggle button element
+const themeToggle = document.getElementById('theme-toggle');
+
+// Function to toggle the dark theme
+function toggleDarkTheme() {
+  document.body.classList.toggle('dark-theme');
+}
+
+// Event listener for the theme toggle button
+themeToggle.addEventListener('click', toggleDarkTheme);
